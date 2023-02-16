96205eaf43066675c6062f044c7467ce1ad94896
Divya R. Gavandi <divya190399@gmail.com>
2023-02-17 02:05:18 +0530

    feat:BubbleSort

---
+++ SortingAlgo/BubbleSort.js
@@ -0,0 +1,24 @@
+let arr = [5, 4, -3, 2, 1];
+console.log("Initial Array - ", arr);
+for (let i = 0; i < arr.length; i++) {
+    let isSwapped = false;
+    for (let j = 0; j < arr.length - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+            swap(j, j + 1);
+            isSwapped = true;
+            console.log("j = ", j, arr);
+        }
+    }
+    if (!isSwapped) {
+        break;
+    }
+    console.log(`Array After ${i + 1}th iteration`);
+    console.log(arr);
+
+}
+
+function swap(arg1, arg2) {
+    let temp = arr[arg1];
+    arr[arg1] = arr[arg2];
+    arr[arg2] = temp;
+}
