8a9f443e732378df32150310265fad710041323b
Divya R. Gavandi <divya190399@gmail.com>
2023-02-17 21:52:58 +0530

    feat:LinearSearch

---
+++ SearchingAlgo/LinearSearch.js
@@ -0,0 +1,18 @@
+const arr = [71, -23, 83, 890, 94, 52, 76, 33, 97];
+const target = 52;
+
+function LinearSearch() {
+    let isPresent = false;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == target) {
+            console.log("Element Found at " + i + "th position");
+            isPresent = true;
+        }
+    }
+
+    if (!isPresent) {
+        console.log("Element Not Prsent in Array");
+    }
+}
+
+LinearSearch();
