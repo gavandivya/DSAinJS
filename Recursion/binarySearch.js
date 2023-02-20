6cf3e62e1137e2980fd417c458a3c655924126eb
Divya R. Gavandi <divya190399@gmail.com>
2023-02-20 20:50:02 +0530

    feat:BinaryUsingRecursion

--- Recursion/binarySearch.js
+++ Recursion/binarySearch.js
@@ -1,6 +1,5 @@
-// const arr = [-87, -67, 0, 23, 34, 45, 56, 64, 72, 78, 81, 99, 102, 145, 189];
-const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-const target = 2;
+const arr = [-87, -67, 0, 23, 34, 45, 56, 64, 72, 78, 81, 99, 102, 145, 189];
+const target = 78;
 
 function binarySearch(start, end) {
     let mid = start + (end - start) / 2;
@@ -8,6 +7,7 @@ function binarySearch(start, end) {
         console.log("Element Not found");
         return -1;
     }
+
 
     if (target == arr[mid]) {
         console.log("Element Found");
