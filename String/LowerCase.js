c08185c6a1ccafe66ddd29a2c82b5063d2fcfcca
Divya R. Gavandi <divya190399@gmail.com>
2023-02-25 03:08:05 +0530

    feat:LowerCase

--- String/LowerCase.js
+++ String/LowerCase.js
@@ -6,12 +6,12 @@ const toLowerCase = () => str.toLowerCase();
 console.log(toLowerCase());
 //Output  - DIVYA CAN DO IT
 
-//Converting the first character of the string to uppercase
+//Converting the first character of the string to lowercase
 const firstLetterLowerCase = () => str.slice(0, 1).toLowerCase() + str.slice(1, str.length);
 console.log(firstLetterLowerCase());
 //Output  - Divya can do it
 
-//Converting only required characters to uppercase without using .UpperCase();
+//Converting only required characters to lowercase without using .toLowerCase();
 const arr = ['A', 'E', 'I', 'O', 'U'];
 let str2 = "dIvyA cAn dO It";
 const requiredCharacters = () => {
@@ -29,13 +29,13 @@ const requiredCharacters = () => {
 console.log(requiredCharacters());
 //Output  - DIVYA CAN DO IT
 
-//Using array to make string uppercase using charCodeAt()
+//Using array to make string lowercase using charCodeAt()
 const usingCharCodeAt = () => {
     let str = 'A(B)CDEF{1}G2HI3GK%!78L';
     let temp = "";
     for (let char of str) {
         let value = char.charCodeAt();
-        if (value >= 64 && value <= 90) { // if lowercase turn it to uppercase
+        if (value >= 64 && value <= 90) { // if uppercase turn it to lowercase
             temp += String.fromCharCode(char.charCodeAt() + 32);
         }
         else {
