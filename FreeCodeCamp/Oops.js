63ace94c07d237cadd3cd7956ff061fcfc49b508
Divya R. Gavandi <divya190399@gmail.com>
2023-03-15 13:28:23 +0530

    feat:OOPS

--- FreeCodeCamp/Oops.js
+++ FreeCodeCamp/Oops.js
@@ -24,3 +24,45 @@ function Dog() {
     this.color = "white";
     this.numLegs = 4;
 }
+
+//Constructor with parameter
+function Dog(name, color) {
+    this.name = name;
+    this.color = color;
+    this.numLegs = 4;
+}
+
+let dog1 = new Dog("Bruno", "White");
+
+//Use Prototype Properties to Reduce Duplicate Code
+
+Dog.prototype.numLegs = 4
+
+
+/**
+ * Iterate Over All Properties
+You have now seen two kinds of properties: own properties and prototype properties.
+ Own properties are defined directly on the object instance itself. 
+And prototype properties are defined on the prototype.
+ */
+
+function Bird(name) {
+    this.name = name;  //own property
+}
+
+Bird.prototype.numLegs = 2; // prototype property
+
+
+let ownProps = [];
+let prototypeProps = [];
+
+for (let property in duck) {
+    if (duck.hasOwnProperty(property)) {
+        ownProps.push(property);
+    } else {
+        prototypeProps.push(property);
+    }
+}
+
+console.log(ownProps);
+console.log(prototypeProps);
