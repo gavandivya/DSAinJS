76360334c5991dcbc6301a3b4f42f36665615018
Divya R. Gavandi <divya190399@gmail.com>
2023-04-05 19:09:08 +0530

    feat:secondlargest

--- StriverSDESheet/ProblemArraysEasy/SecondLargest.js
+++ StriverSDESheet/ProblemArraysEasy/SecondLargest.js
@@ -1,8 +1,54 @@
-const secondlargest = (arr) => {
-    for (let i = 0; i < arr.length; i++) {
+const arr = [1, 2, 4, 7, 7, 5];
 
+//way1
+const secondLargestSort = (array) => {
+    let largest = array.sort((a, b) => a - b)[array.length - 1];
+    for (let i = array.length - 2; i >= 0; i--) {
+        if (array[i] !== largest) {
+            return array[i];
+        }
     }
-
 }
 
-console.log(secondlargest([4, 78, 32, 56, 99, 1, 101]))
+console.log(secondLargestSort(arr));
+//worst case would be array with same elements-> [7,7,7,7,7,7,7]
+
+//way2
+const secondlargest2Loops = (arr) => {
+    let largest = arr[0];
+    let secondLargest = -1;
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+    return secondLargest;
+}
+
+console.log(secondlargest2Loops(arr));
+//TC - (2N)
+
+
+//way3
+const secondlargest = (arr) => {
+    let largest = arr[0];
+    let secondLargest = -1;
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] < largest && arr[i] > secondLargest) {
+            secondLargest = arr[i];
+        }
+    }
+    return secondLargest;
+}
+
+console.log(secondlargest(arr));
+//TC - O(N)
