55c3355dd9b31c90988e0a80d7c83cf4521a0415
Divya R. Gavandi <divya190399@gmail.com>
2023-04-07 01:25:19 +0530

    feat:LeftShiftbyN

--- StriverSDESheet/ProblemArraysEasy/LeftRotateArrayByOneplace.js
+++ StriverSDESheet/ProblemArraysEasy/LeftRotateArrayByOneplace.js
@@ -1,4 +1,4 @@
-const arr = [1, 2, 3, 4, 5];
+const arr = [1, 2, 3, 4, 5, 6];
 
 //way1
 const rotate = (array) => {
@@ -10,6 +10,27 @@ const rotate = (array) => {
     return array;
 }
 
-console.log(rotate(arr));
+// console.log(rotate(arr));
 //TC - O(N)
 //SC- O(1)
+
+
+//for shifting left by n numbers
+const rotateByD = (array, d, n) => {
+    if (d > n) {
+        d = d % n;
+    }
+    let temp = array.slice(0, d);
+    console.log(temp);
+    for (let i = 0; i < n - d; i++) {
+        console.log(array[i], array[i + d]);
+        array[i] = array[i + d];
+    }
+    console.log(array);
+    for (let i = n - d; i < n; i++) {
+        array[i] = temp[i + d - n];
+    }
+    return array;
+}
+
+console.log(rotateByD(arr, 2, 6));
