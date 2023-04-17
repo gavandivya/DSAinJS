f47fb3cccf89c854e97af75fa621e6bdbeb9db29
Divya R. Gavandi <divya190399@gmail.com>
2023-04-17 12:45:30 +0530

    feat:MissingElements

--- StriverSDESheet/ProblemArraysEasy/MissingElement.js
+++ StriverSDESheet/ProblemArraysEasy/MissingElement.js
@@ -1,8 +1,9 @@
-const array = [1, 2, 3, 4, 5, 7, 8, 9];
+// const array = [1, 2, 3, 4, 5, 7, 8, 9];
+const array = [9, 6, 4, 2, 3, 5, 7, 0, 1]
 //way1 linear search
 
 const missingElement = () => {
-    for (let i = 1; j <= array.length; i++) {
+    for (let i = 1; i <= array.length; i++) {
         let flag = 0;
         for (let j = 0; j < array.length; j++) {
             if (i == array[j]) {
@@ -17,28 +18,41 @@ const missingElement = () => {
 
 }
 
-
 console.log(missingElement());
 //TC - O(N*N)
 //SC - O(1)
 
+//array should be in sorted array
 
-const missingElements = () => {
-    let i = 1;
-    for (let j = 0; j < array.length; j++) {
-        if (i !== array[j]) {
-            return i;
-        }
-        i++;
-    }
-}
 
-console.log(missingElements());
+
+// console.log(missingElements());
 //TC - O(N)
 //SC - O(1)
 
 //way2 hash method
+// const array = [9, 6, 4, 2, 3, 5, 7, 0, 1]
+//[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+const missingHash = () => {
+    let temp = [];
+    for (let i = 0; i <= array.length; i++) {
+        temp[i] = 0; //[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    }
 
+    for (let i = 0; i < array.length; i++) {
+        temp[array[i]] = 1;
+    }
+
+    let ans = 0;
+    for (i = 0; i < array.length; i++) {
+        if (temp[i] == 0) {
+            ans = i;
+        }
+    }
+    return ans;
+}
+
+console.log(missingHash());
 
 //way3 using XOR
 const missingUsingXOR = () => {
@@ -47,16 +61,14 @@ const missingUsingXOR = () => {
     // for (let i = 1; i <= array.length + 1; i++) {
     //     xor1 = xor1 ^ i;
     // }
-
     for (let i = 0; i < array.length; i++) {
         xor1 = xor1 ^ (i + 1);// we can use same loop
         xor2 = xor2 ^ array[i];
     }
-
     return xor1 ^ xor2;
 }
 
-// console.log(missingUsingXOR());
+console.log(missingUsingXOR());
 //TC - O(2N) but we can put xor1 in 2nd loop
 //SC - O(1)
 
@@ -72,7 +84,7 @@ const missingEle = () => {
     return sum - s2;
 }
 
-// console.log(missingEle());
+console.log(missingEle());
 //This approach takes sligtly more space has int can take (10^5 X 10^5+1)/2 so long will be needed
 //Time complexity - O(N)
 //Space complexity - O(1)
