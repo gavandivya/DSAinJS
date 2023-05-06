07a6236b38c91a4aeb3a8a80612a595beb1112b2
Divya R. Gavandi <divya190399@gmail.com>
2023-05-06 15:07:23 +0530

    feat:comment added

--- BitwiseOperator/OddEven.js
+++ BitwiseOperator/OddEven.js
@@ -1,6 +1,7 @@
 const isOdd = (n) => {
-    return (n & 1) == 1;
+    return (n & 1) == 1; //using bitwise operator
 }
 
 console.log(isOdd(7) ? "Odd" : "Even");
-
+//As we know bitwise AND Operation of the Number by 1 will be 1, If it is odd because the last bit will be already set. 
+//Otherwise, it will give 0 as output.
