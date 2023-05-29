1a85c6a8d3405fba2ea2c56fdb56cdfdf588617a
Divya R. Gavandi <divya190399@gmail.com>
2023-05-29 18:20:08 +0530

    feat:kLength

---
+++ String/klengthString.js
@@ -0,0 +1,15 @@
+//Chop string into chunks of given length
+//Write a function to chop string into chunks of given length and return it as array.
+
+
+/**
+Input:
+'javascript'
+3
+
+Output:
+['jav','asc','rip','t']
+ */
+
+const string = "javascript";
+let array = [];
