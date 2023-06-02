3e3e01282c03546733ddbf812106d57b57c45b17
Divya R. Gavandi <divya190399@gmail.com>
2023-06-02 18:16:14 +0530

    feat:arrays

---
+++ RoadsideCoder/array.js
@@ -0,0 +1,12 @@
+/**
+ * Mutating method	    Non-mutating alternative
+    copyWithin()	    No one-method alternative
+    fill()	            No one-method alternative
+    pop()	            slice(0, -1)
+    push(v1, v2)	    concat([v1, v2])
+    reverse()	        toReversed()
+    shift()	            slice(1)
+    sort()	            toSorted()
+    splice()	        toSpliced()
+    unshift(v1, v2)	    toSpliced(0, 0, v1, v2)
+ */
