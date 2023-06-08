ef944453612e5635d46f6fe2844c9e8884b898ff
Divya R. Gavandi <divya190399@gmail.com>
2023-06-09 01:20:29 +0530

    feat:arrays

--- RoadsideCoder/array.js
+++ RoadsideCoder/array.js
@@ -13,3 +13,81 @@
  */
 
 //Implementation Part
+
+
+//Array Declaration
+
+//way1
+let array1 = new Array();
+//way2
+let array = [];
+
+let person = {
+    name: "Raju",
+    age: 55
+}
+
+/**
+ * adding element at end  
+ * 1.   push()
+ */
+array.push('Divya');
+array.push('Disha', 'Bruno')
+console.log(array);
+
+/**adding element at start 
+ * 2.   unshift()
+ * */
+array.unshift('Rashmi', person);
+console.log(array);
+
+//accessing first array element 
+console.log(array[0]);
+console.log(array[1].name);
+
+//no of elements in array
+console.log(array.length);
+
+/**removing elemnts from start
+ * 3.   pop()
+ * */
+array.pop();
+
+/**removing elemnts from end
+ * 4.   shift()
+ * */
+array.shift();
+
+/**
+ * Looping an array
+ */
+
+//for
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
+
+//while
+let i = 0;
+while (i < array.length) {
+    console.log(array[i]);
+    i++;
+}
+
+
+let arr = [1, 2, 3, 4, 5]
+/* 5.   map() use to make changes to whole array */
+const newNums = arr.map((item, index, array) => {
+    return item + 5;
+})
+
+console.log(newNums);
+
+/* 5.   filter() allows us to add condition and return those elements that satisfy the conditions*/
+
+const filterNum = arr.filter((item) => {
+    return item > 3
+})
+
+console.log(filterNum);
+
