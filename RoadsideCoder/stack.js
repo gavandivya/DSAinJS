c4cd63cc8566542566af00af7f914f2c37215cc9
Divya R. Gavandi <divya190399@gmail.com>
2023-06-13 00:30:32 +0530

    feat:stack

--- RoadsideCoder/stack.js
+++ RoadsideCoder/stack.js
@@ -3,4 +3,34 @@
 
 
 //balance parentesis
+const bracket = "[({})]"
+let stack = [];
 
+const validPar = (string) => {
+    for (let i = 0; i < string.length; i++) {
+        let char = string[i];
+
+        if (char === '[' || char === '(' || char === '{') {
+            stack.push(char);
+        }
+        else if (char === ']' || char === '}' || char === ')') {
+            if (isEmpty(stack)) { // case where no opening bracket
+                return false;
+            }
+            let top = stack.pop();
+            if ((top === '{' && char !== '}') || (top === '(' && char !== ')') || (top === '[' && char !== ']')) {
+                return false;
+            }
+        }
+
+    }
+
+    return isEmpty(stack);
+}
+
+
+const isEmpty = () => {
+    return stack.length === 0;
+}
+
+console.log(validPar(bracket));
