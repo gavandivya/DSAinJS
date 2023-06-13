350ff63c9fa9ad87e21228d19cf2398feced23bd
Divya R. Gavandi <divya190399@gmail.com>
2023-06-14 01:44:35 +0530

    feat:stack

--- RoadsideCoder/kadane.js
+++ RoadsideCoder/kadane.js
@@ -1,1 +1,21 @@
 //find out the largest sum of subarray from the array
+
+const array = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+let maxSum = array[0];
+//brute force approach
+
+for (let i = 0; i < array.length; i++) {
+    let currentSum = 0;
+    for (let j = i; j < array.length; j++) {
+        currentSum = currentSum + array[j];
+        if (currentSum > maxSum) {
+            maxSum = currentSum;
+        }
+    }
+}
+console.log(maxSum);
+//Time Complexity - O(n^2)
+
+
+//using kadane 
+
