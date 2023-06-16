9b1bf2ae6d8de905fa914fddd0db3ae99509dc99
Divya R. Gavandi <divya190399@gmail.com>
2023-06-17 00:41:15 +0530

    feat:stack

--- RoadsideCoder/stack.js
+++ RoadsideCoder/stack.js
@@ -1,3 +1,46 @@
+//Stack Implementation
+
+class Stack {
+    constructor() {
+        this.stack = [];
+    }
+
+    push(element) {
+        this.stack.push(element);
+    }
+
+    pop() {
+        if (this.isEmpty()) {
+            return "Stack is empty"
+        }
+        this.stack.pop();
+    }
+
+    peek() {
+        if (this.isEmpty()) {
+            return "Stack is empty"
+        }
+        return this.stack[this.size() - 1];
+    }
+
+    size() {
+        return this.stack.length;
+    }
+
+    isEmpty() {
+        return this.size === 0;
+    }
+}
+
+const stack1 = new Stack();
+stack1.push(24);
+stack1.push(42);
+stack1.push(55);
+stack1.push(78);
+
+console.log(stack1.peek());
+console.log(stack1.pop());
+console.log(stack1);
 
 //Given a input string s, reverse a order of words
 const reverseString = (str) => {
