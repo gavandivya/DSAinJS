88aecf908f4ae6d46840e293935581041469d16d
gavandivya <divya190399@gmail.com>
2023-08-02 22:25:42 +0530

    feat:string

--- RoadsideCoder/string.js
+++ RoadsideCoder/string.js
@@ -1,1 +1,21 @@
 // String methods and leetcode questions
+//Methods
+/*
+replace()
+replaceAll()
+trim()
+indexOf()
+charAt()
+length()
+concat()
+lastIndexOf()
+startsWith()
+endsWith()
+slice()
+toUpperCase()
+toLowerCase()
+includes()
+join()
+split()
+localeCompare()
+*/ 
