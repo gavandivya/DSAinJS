e052abb5e4b2b2b89402593380469ca16fe48440
gavandivya <divya190399@gmail.com>
2023-08-18 21:29:24 +0530

    feat:FloydTriangle

--- Patterns/Pattern.js
+++ Patterns/Pattern.js
@@ -431,3 +431,30 @@ pattern03(5);
  **/
  
 
+//Floyd's triangle
+function patternFloydTriangle(n) {
+    let string = "";
+    let count = 0;
+    for(let i = 0; i<n;i++){
+        string = " "
+        for(let j =0;j<i+1;j++){
+        count = count+1;
+        string = string +count+" "
+        }
+        console.log(string);
+    }
+    }
+    patternFloydTriangle(8)
+       
+    /** Output 
+    
+     1 
+     2 3 
+     4 5 6 
+     7 8 9 10 
+     11 12 13 14 15 
+     16 17 18 19 20 21 
+     22 23 24 25 26 27 28 
+     29 30 31 32 33 34 35 36 
+     
+     **/
