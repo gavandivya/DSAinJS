9ed89d70b15c29b6a8cc723df51a701d738488c2
gavandivya <divya190399@gmail.com>
2023-09-02 01:30:14 +0530

    feat:Hourglass

--- Patterns/Pattern.js
+++ Patterns/Pattern.js
@@ -644,3 +644,36 @@ pattern07(5);
          *   *       
            *       
  */
+console.log("----------------------------");
+function HourGlass(n) {
+    let string = "";
+    for (let i = 0; i < n; i++) {
+      string = " ";
+      for (let j = 0; j <= i; j++) {
+        string = string + "  ";
+      }
+      for (let j = 0; j <= 2 * n - 2 * i - 2; j++) {
+        string = string + "* ";
+      }
+      for (let j = 0; j <= i; j++) {
+        string = string + "  ";
+      }
+  
+      console.log(string);
+    }
+    for (let i = 0; i < n; i++) {
+      string = " ";
+      for (let j = 0; j < n - i; j++) {
+        string = string + "  ";
+      }
+      for (let j = 0; j <= 2 * i; j++) {
+        string = string + "* ";
+      }
+      for (let j = 0; j < n - i; j++) {
+        string = string + "  ";
+      }
+  
+      console.log(string);
+    }
+  }
+  HourGlass(4);
