81c198b4e5e3fb99fd105b057b7f9d1e7db25dab
gavandivya <divya190399@gmail.com>
2023-09-18 21:45:21 +0530

    feat:TwoPinter:twoSum

--- TwoPointer/TwoPointer.js
+++ TwoPointer/TwoPointer.js
@@ -1,24 +1,21 @@
 // For two sum using two pointer approach array needs to be sorted
 
-const twoSum = (arr,target) =>{
-    let left = 0;
-    let right = arr.length-1;
-    while(left < right){
-        let sum = arr[left]+arr[right]
-        if(sum == target){
-            console.log(arr[left],arr[right]);
-            return;
-        }
-        else if(sum < target){
-            left++;
-        }
-        else if(sum> target){
-            right--;
-        }
+const twoSum = (arr, target) => {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum == target) {
+      console.log(arr[left], arr[right]);
+      return;
+    } else if (sum < target) {
+      left++;
+    } else if (sum > target) {
+      right--;
     }
-    console.log("Not found");
+  }
+  console.log("Not found");
+};
 
-}
-
-twoSum([1,4,7,9,12,14],19);
+twoSum([1, 4, 7, 9, 12, 14], 19);
 //Output - 7 12
