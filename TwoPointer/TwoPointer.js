380cf5c1fe9d93a20cdc75235bceff86ee450140
gavandivya <divya190399@gmail.com>
2023-09-19 21:38:13 +0530

    feat:TwoPinter:twoSum

---
+++ TwoPointer/TwoPointer.js
@@ -0,0 +1,21 @@
+const twoSum = (arr,target) =>{
+    let left = 0;
+    let right = arr.length-1;
+    while(left < right){
+        let sum = arr[left]+arr[right]
+        if(sum == target){
+            console.log(arr[left],arr[right]);
+            return;
+        }
+        else if(sum < target){
+            left++;
+        }
+        else if(sum> target){
+            right--;
+        }
+    }
+    console.log("Not found");
+
+}
+
+twoSum([1,4,7,9,12,14],19);
